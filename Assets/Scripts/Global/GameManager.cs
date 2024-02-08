a7011b9775fe1d79252963977a5a471401f5c363
Amberjack10 <sanghwasong10@gmail.com>
2024-02-08 17:21:22 +0900

    feat : StageManager 생성

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,9 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    public event Action OnStageClear;
+    public event Action OnStageOver;
+
+    // 제한 시간
+    public float limitTime = 60.0f;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +27,15 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        limitTime -= Time.deltaTime;
+
+        // 제한 시간 초과 시
+        if(limitTime <= 0)
+        {
+            OnStageOver?.Invoke();
+        }
     }
+
+    // TODO : 스테이지 해금 및 클리어
+    // TODO : 자원 관리
 }
