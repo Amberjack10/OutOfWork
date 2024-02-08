c6f419142c559d92fef440b485329b44e6ece3b4
chai227chai <chai227chai@naver.com>
2024-02-08 21:03:01 +0900

    [feat] : Unit Base 기초적인 행동 작업

--- Assets/Unit_Folder/Unit_Scripts/UnitBase.cs
+++ Assets/Unit_Folder/Unit_Scripts/UnitBase.cs
@@ -13,12 +13,19 @@ public enum UnitState
 
 public class UnitBase : MonoBehaviour
 {
-    protected bool isPlayerUnit;
-    protected bool foundEnemy = false;
+    protected static readonly int IsWalking = Animator.StringToHash("IsWalking");
+    protected static readonly int Attack = Animator.StringToHash("Attack");
+    protected static readonly int Dead = Animator.StringToHash("Dead");
+
+    protected string opponentTag;
+    [SerializeField]protected bool isPlayerUnit;
+    [SerializeField]protected bool foundEnemy = false;
 
     private Vector2 moveDirection;
     private Rigidbody2D _rigidbody;
-    public LayerMask layermask;
+
+    protected Animator animator;
+    protected SpriteRenderer sprite;
 
     protected UnitState state;
 
@@ -27,36 +34,85 @@ public class UnitBase : MonoBehaviour
     public float walkSpeed;
     public int attack;
     public float attackRate;
-    private float attackLastTime;
+    private bool canAttack = true;
 
-    private Collider2D attackRange;
+    private CircleCollider2D attackRange;
 
     protected void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        
+        animator = GetComponentInChildren<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     protected void Start()
     {
-        state = UnitState.Idle;
-        attackRange = transform.Find("AttackRange").GetComponent<Collider2D>();
+        SetState(UnitState.Idle);
+        attackRange = transform.Find("AttackRange").GetComponent<CircleCollider2D>();
+        SetOppoenet();
     }
 
     protected void Update()
     {
-        
+        switch (state)
+        {
+            case UnitState.Idle:
+                UpdatePassive();
+                break;
+            case UnitState.Walking:
+                UpdateWalking();
+                break;
+            case UnitState.Attacking:
+                UpdateAttacking();
+                break;
+        }
+    }
+
+    private void SetOppoenet()
+    {
+        if (gameObject.CompareTag("PlayerUnit"))
+        {
+            isPlayerUnit = true;
+            opponentTag = "EnemyUnit";
+        }
+        else if (gameObject.CompareTag("EnemyUnit"))
+        {
+            isPlayerUnit = false;
+            opponentTag = "PlayerUnit";
+        }
+    }
+
+    private void UpdateAttacking()
+    {
+        if (foundEnemy)
+        {
+            if (canAttack)
+            {
+                canAttack = false;
+                animator.SetTrigger(Attack);
+                Invoke("WaitAttackCoolTime", attackRate);
+            }
+        }
+        else
+        {
+            SetState(UnitState.Idle);
+        }
+    }
+
+    protected void WaitAttackCoolTime()
+    {
+        canAttack = true;
     }
 
     protected void UpdatePassive()
     {
         if (foundEnemy)
         {
-            state = UnitState.Attacking;
+            SetState(UnitState.Attacking);
         }
         else
         {
-            state = UnitState.Walking;
+            SetState(UnitState.Walking);
         }
     }
 
@@ -64,7 +120,7 @@ public class UnitBase : MonoBehaviour
     {
         if (foundEnemy)
         {
-            state = UnitState.Attacking;
+            SetState(UnitState.Attacking);
         }
         else
         {
@@ -74,16 +130,38 @@ public class UnitBase : MonoBehaviour
 
     public void Move_Unit()
     {
-        if(isPlayerUnit == false)
+        if(!isPlayerUnit)
         {
             moveDirection = Vector2.left;
+            sprite.flipX = true;
         }
         else
         {
             moveDirection = Vector2.right;
+            sprite.flipX = false;
         }
 
         _rigidbody.velocity = moveDirection * walkSpeed;
+    }
+
+    protected void SetState(UnitState newState)
+    {
+        state = newState;
+
+        switch (state)
+        {
+            case UnitState.Idle:
+                _rigidbody.velocity = Vector2.zero;
+                animator.SetBool(IsWalking, false);
+                break;
+            case UnitState.Walking:
+                animator.SetBool(IsWalking, true);
+                break;
+            case UnitState.Attacking:
+                _rigidbody.velocity = Vector2.zero;
+                animator.SetBool(IsWalking, false);
+                break;
+        }
     }
 
     public void TakeDamage(int damage)
@@ -98,5 +176,34 @@ public class UnitBase : MonoBehaviour
     private void Die()
     {
         Destroy(gameObject);
+    }
+
+    protected void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag(opponentTag))
+        {
+            foundEnemy = true;
+        }
+    }
+
+    protected void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(opponentTag))
+        {
+            foundEnemy = false;
+        }
+    }
+
+    protected void OnHit()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(attackRange.transform.position, attackRange.radius, 0);
+
+        foreach(Collider2D collider in colliders)
+        {
+            if (collider.CompareTag(opponentTag))
+            {
+                collider.GetComponent<UnitBase>().TakeDamage(attack);
+            }
+        }
     }
 }
