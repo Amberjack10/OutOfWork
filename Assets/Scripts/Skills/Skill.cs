6866ba030fc201dcd7c0453ac6a50d62eff7b308
purangi <kaya0728@naver.com>
2024-02-14 16:28:45 +0900

    feat: SoundManager과 AudioMixer 구현

---
+++ Assets/Scripts/Skills/Skill.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        transform.localPosition = new Vector3(-25f, -2.5f, 0);
+    }
+    void Start()
+    {
+        //transform.localPosition = new Vector3(-25f, -2.5f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("EnemyUnit"))
+        {
+            collision.gameObject.GetComponent<IDamageable>().TakeDamage(SkillManager.instance.atk);
+            StartCoroutine(Destroy());
+        }
+    }
+
+    IEnumerator Destroy()
+    {
+        anim.SetTrigger("Destroy");
+        yield return new WaitForSeconds(0.3f);
+        Destroy(gameObject);
+    }
+}
